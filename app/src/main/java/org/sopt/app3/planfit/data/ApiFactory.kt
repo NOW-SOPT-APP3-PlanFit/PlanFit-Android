b9ecfa470aeb216675c6e303a05f958552d6caef
Sangwook123 <operawook@catholic.ac.kr>
2024-05-17 20:32:35 +0900

    [feat] #17 add interceptor

--- app/src/main/java/org/sopt/app3/planfit/data/ApiFactory.kt
+++ app/src/main/java/org/sopt/app3/planfit/data/ApiFactory.kt
@@ -5,15 +5,20 @@ import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.sopt.app3.planfit.data.api.ExerciseListService
+import org.sopt.app3.planfit.data.interceptor.AuthInterceptor
 import retrofit2.Retrofit
 
 object ApiFactory {
     private const val BASE_URL = "BuildConfig.BASE_URL"
 
     private val client by lazy {
-        OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().apply {
-            level = HttpLoggingInterceptor.Level.BODY
-        }).build()
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .addInterceptor(AuthInterceptor())
+            .build()
     }
 
     val retrofit: Retrofit by lazy {
@@ -24,4 +29,8 @@ object ApiFactory {
 
 
     inline fun <reified T> create(): T = retrofit.create<T>(T::class.java)
+}
+
+object ServicePool {
+    val exerciseListService by lazy { ApiFactory.create<ExerciseListService>() }
 }
---
+++ app/src/main/java/org/sopt/app3/planfit/data/interceptor/AuthInterceptor.kt
@@ -0,0 +1,10 @@
+package org.sopt.app3.planfit.data.interceptor
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        return chain.proceed(chain.request().newBuilder().addHeader("userId","1").build())
+    }
+}
