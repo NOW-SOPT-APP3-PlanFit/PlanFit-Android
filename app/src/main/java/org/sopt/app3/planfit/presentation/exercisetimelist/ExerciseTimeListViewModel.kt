0d4f57fc14f2fe235965bf459d864c0e6e8c8322
Hyoeun <137873124+hyoeunjoo@users.noreply.github.com>
2024-05-17 20:04:26 +0900

    [feat] #15 바텀 시트 ViewModel 구현

---
+++ app/src/main/java/org/sopt/app3/planfit/presentation/exercisetimelist/ExerciseTimeListViewModel.kt
@@ -0,0 +1,15 @@
+package org.sopt.app3.planfit.presentation.exercisetimelist
+
+import org.sopt.app3.planfit.domain.model.ExerciseTime
+
+class ExerciseTimeListViewModel {
+        val mock: List<ExerciseTime> = listOf(
+            ExerciseTime("짧게","약 29분"),
+            ExerciseTime("짧게2","약 29분"),
+            ExerciseTime("짧게3","약 29분"),
+            ExerciseTime("짧게4","약 29분"),
+            ExerciseTime("짧게5","약 29분"),
+            ExerciseTime("짧게6","약 29분"),
+
+        )
+    }
