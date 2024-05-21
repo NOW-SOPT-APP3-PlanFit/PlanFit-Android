f89d1771fbe92c60c95dfd76932312efa63bf839
윤서희 <appleid21@sookmyung.ac.kr>
2024-05-21 18:05:15 +0900

    [feat] #19 stopwatch ui

---
+++ app/src/main/java/org/sopt/app3/planfit/presentation/StopwatchActivity.kt
@@ -0,0 +1,21 @@
+package org.sopt.app3.planfit.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.sopt.app3.planfit.R
+
+class StopwatchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_stopwatch)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
