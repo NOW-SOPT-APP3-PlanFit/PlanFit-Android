72d99ae39a9e3332fe1d20947170ed0a6a8cf3b0
Hyoeun <137873124+hyoeunjoo@users.noreply.github.com>
2024-05-22 17:37:55 +0900

    [mod] #23 구현 코드 변경

--- app/src/main/java/org/sopt/app3/planfit/presentation/StretchingMainActivity.kt
+++ app/src/main/java/org/sopt/app3/planfit/presentation/StretchingMainActivity.kt
@@ -1,6 +1,7 @@
 package org.sopt.app3.planfit.presentation
 
 import android.content.Intent
+import android.graphics.drawable.Animatable
 import android.os.Bundle
 import android.os.CountDownTimer
 import coil.ImageLoader
@@ -9,7 +10,6 @@ import coil.load
 import org.sopt.app3.planfit.R
 import org.sopt.app3.planfit.core.ui.base.BaseActivity
 import org.sopt.app3.planfit.databinding.ActivityStretchingMainBinding
-
 
 class StretchingMainActivity : BaseActivity<ActivityStretchingMainBinding>({ inflater ->
     ActivityStretchingMainBinding.inflate(inflater)
@@ -26,8 +26,14 @@ class StretchingMainActivity : BaseActivity<ActivityStretchingMainBinding>({ inf
         setUpTimer()
 
         binding.btnProgressStop.setOnClickListener {
-            toggleTimer()
+            if (isTimerRunning) {
+                stopTimer()
+            } else {
+                startTimer()
+            }
+            updateButtonImage()
         }
+
         binding.btnProgressNext.setOnClickListener {
             moveToExerciseMainActivity()
         }
@@ -46,6 +52,7 @@ class StretchingMainActivity : BaseActivity<ActivityStretchingMainBinding>({ inf
     }
 
     private fun startTimer() {
+        loadGif()
         val remainingTime = totalTime - timeElapsed
         timer = object : CountDownTimer(remainingTime, 1000) {
             override fun onTick(untilFinished: Long) {
@@ -54,49 +61,48 @@ class StretchingMainActivity : BaseActivity<ActivityStretchingMainBinding>({ inf
                 val minutes = secondsElapsed / 60
                 val seconds = secondsElapsed % 60
 
-                binding.tvTimeMinute.setText(String.format("%02d", minutes))
-                binding.tvTimeSecond.setText(String.format("%02d", seconds))
+                binding.tvTimeMinute.text = String.format("%02d", minutes)
+                binding.tvTimeSecond.text = String.format("%02d", seconds)
                 binding.pbProgressBar.progress = timeElapsed.toInt()
             }
 
             override fun onFinish() {
-                binding.tvTimeMinute.setText("03")
-                binding.tvTimeSecond.setText("00")
+                binding.tvTimeMinute.text = "03"
+                binding.tvTimeSecond.text = "00"
                 binding.pbProgressBar.progress = totalTime
                 timeElapsed = 0
             }
         }
         timer?.start()
         isTimerRunning = true
-        loadGif(true)
+        with(binding.ivExerciseStretching) {
+            drawable.let {
+                if (it is Animatable) {
+                    it.start()
+                }
+            }
+        }
     }
 
     private fun stopTimer() {
         timer?.cancel()
         isTimerRunning = false
-        loadGif(false)
-    }
-
-    private fun toggleTimer() {
-        if (isTimerRunning) {
-            stopTimer()
-        } else {
-            startTimer()
-        }
-        updateButtonImage()
-    }
-
-    private fun loadGif(started: Boolean) {
-        if (started) {
-            val imageLoader = ImageLoader.Builder(this)
-                .components {
-                    add(ImageDecoderDecoder.Factory())
+        with(binding.ivExerciseStretching) {
+            drawable.let {
+                if (it is Animatable) {
+                    it.stop()
                 }
-                .build()
-            binding.ivExerciseStretching.load(R.raw.gif_exc_cycle, imageLoader = imageLoader)
-        } else {
-            binding.ivExerciseStretching.setImageResource(R.drawable.gif_exc_cycle)
+            }
         }
+    }
+
+    private fun loadGif() {
+        val imageLoader = ImageLoader.Builder(this)
+            .components {
+                add(ImageDecoderDecoder.Factory())
+            }
+            .build()
+        binding.ivExerciseStretching.load(R.raw.gif_exc_cycle, imageLoader = imageLoader)
     }
 
     private fun updateButtonImage() {
