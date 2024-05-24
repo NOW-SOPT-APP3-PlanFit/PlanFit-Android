a26a19514576f31c97dce70f8ef9a2cb8656aa5f
Hyoeun <137873124+hyoeunjoo@users.noreply.github.com>
2024-05-24 14:04:11 +0900

    [chore] #29 코드 정리

--- app/src/main/java/org/sopt/app3/planfit/presentation/MainActivity.kt
+++ app/src/main/java/org/sopt/app3/planfit/presentation/MainActivity.kt
@@ -1,32 +1,18 @@
 package org.sopt.app3.planfit.presentation
 
-import android.app.Dialog
 import android.os.Bundle
-import android.widget.Adapter
 import android.widget.Button
-import android.widget.FrameLayout
-import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import org.sopt.app3.planfit.R
-import org.sopt.app3.planfit.presentation.exerciselist.ExerciseViewModel
 import org.sopt.app3.planfit.presentation.exercisetimelist.ExerciseConditionBottomSheetFragment
 import org.sopt.app3.planfit.presentation.exercisetimelist.ExerciseTimeBottomSheetFragment
-import org.sopt.app3.planfit.presentation.exercisetimelist.ExerciseTimeListViewModel
-import org.sopt.app3.planfit.presentation.exercisetimelist.adapter.ExerciseTimeListAdapter
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         val btnTime = findViewById<Button>(R.id.btn_minute)
         btnTime.setOnClickListener {
@@ -36,7 +22,6 @@ class MainActivity : AppCompatActivity() {
                 selectedTime = 0
             )
             fragment.show(supportFragmentManager, fragment.tag)
-
         }
 
         val btnCondition = findViewById<Button>(R.id.btn_condition)
@@ -47,9 +32,6 @@ class MainActivity : AppCompatActivity() {
                 selectedCondition = "1"
             )
             fragment.show(supportFragmentManager, fragment.tag)
-
         }
     }
-
-
 }
